205837c52f3ecbe74567685845c88c499cf4fbf0
Thomas Gabor <github@das.ist.denkfrei.de>
2013-11-21 05:01:17 +0100

    minor extension of akademia2 test code

--- Scenarios/akademia2.lisp
+++ Scenarios/akademia2.lisp
@@ -3,6 +3,25 @@
 (define-class logic-space (spondeios:hexameter-space)
   ())
 
+;; (defmethod spondeios:handle ((self logic-space) msgtype author space parameter
+;;                    &optional recipient)
+;;   (values
+;;    (list (hex:hex-item 
+;;           :answer (list (hex-item 
+;;                          :type "move"
+;;                          :control (hex-item
+;;                                    :up 1))
+;;                         (hex-item
+;;                          :type "shout"
+;;                          :control (hex-item
+;;                                    :content (hex-item
+;;                                              :class "commit goal"
+;;                                              :live (list (hex-item
+;;                                                           :class "motor"
+;;                                                           :type "move"
+;;                                                           :control (hex-item
+;;                                                                     :right 1)))))))))
+;;    t))
 (defmethod spondeios:handle ((self logic-space) msgtype author space parameter
                    &optional recipient)
   (values
@@ -17,10 +36,18 @@
                                    :content (hex-item
                                              :class "commit goal"
                                              :live (list (hex-item
-                                                          :class "motor"
-                                                          :type "move"
-                                                          :control (hex-item
-                                                                    :right 1)))))))))
+                                                          :class "commit feature"
+                                                          1 (hex-item :on (hex-item :x 7)
+                                                                    :do (hex-item
+                                                                         :class "motor"
+                                                                         :type "move"
+                                                                         :control (hex-item
+                                                                                   :up 1)))
+                                                          :otherwise (hex-item
+                                                                      :class "motor"
+                                                                      :type "move"
+                                                                      :control (hex-item
+                                                                                :right 1))))))))))
    t))
 (let ((context (hex:init :me "localhost:55559" :space 'logic-space)))
   (format t "~&Entering infinite response loop, please abort manually.")
